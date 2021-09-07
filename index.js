25ffb22e4aece0ded5f545be93280633de49cdc6
nwolfe4 <89557875+nwolfe4@users.noreply.github.com>
2021-09-07 01:37:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// load core http mod
+
+const http = require("http");
+
+// load core filesystem mod, 
+
+// Promises -alternative to callbacks that doesn't jam up server per request (async)
+
+const fs = require('fs').promises;
+
